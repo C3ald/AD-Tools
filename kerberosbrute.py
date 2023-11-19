d6298cac4df9c84770bcb639e83182faedb45448
C3ald <specklesjr10@gmail.com>
2023-11-18 21:16:17 -0700

    added ldap connection

--- kerberosbrute.py
+++ kerberosbrute.py
@@ -2,3 +2,15 @@ from queue import Queue
 import time as t
 from multiprocessing.dummy import Pool as ThreadPool
 import argparse
+from impacket import version
+from impacket.examples import logger
+from impacket.krb5.kerberosv5 import getKerberosTGT, KerberosError, SessionKeyDecryptionError
+from impacket.krb5 import constants
+from impacket.krb5.types import Principal
+from impacket.krb5.ccache import CCache
+from impacket.ldap import ldap, ldapasn1, ldaptypes
+from impacket.krb5 import constants
+from impacket.krb5.asn1 import AS_REQ, KERB_PA_PAC_REQUEST, KRB_ERROR, AS_REP, seq_set, seq_set_iter
+from impacket.krb5.kerberosv5 import sendReceive, KerberosError
+from impacket.krb5.types import KerberosTime, Principal
+from utils.adconn import LdapConn
