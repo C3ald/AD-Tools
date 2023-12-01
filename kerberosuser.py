62177893d4d8c4fc7c80793f9478b708cd1e1eda
C3ald <specklesjr10@gmail.com>
2023-11-30 17:30:29 -0700

    trying

--- kerberosuser.py
+++ kerberosuser.py
@@ -36,8 +36,13 @@ def enumerate_user(user, domain, dc):
     dc_ip = socket.gethostbyname(dc)
     userclient = Principal(user, type=constants.PrincipalNameType.NT_PRINCIPAL.value)
     try:
-        getKerberosTGT(userclient,domain=domain,kdcHost=dc_ip, password='',
+        try:
+            getKerberosTGT(userclient,domain=domain,kdcHost=dc_ip, password='',
                            lmhash='', nthash='')
+        except:
+            
+            nopreauthTGT(userclient,domain=domain,kdcHost=dc_ip, password='',
+                           lmhash='', nthash='', kerberoast_no_preauth=True)
     except SessionError as e:
         code = e.getErrorCode()
         if code != 6:
